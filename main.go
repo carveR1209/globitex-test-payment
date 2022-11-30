9d8e1745cc91fbcd6fcef2b74080d0bea180d78f
zakevicius <m.zakevicius@gmail.com>
2022-11-30 12:00:56 +0200

    Set body manually

--- main.go
+++ main.go
@@ -96,7 +96,7 @@ func makePayment() {
 	spew.Dump(headers)
 
 	response, err := resty.New().R().
-		SetBody(strings.Replace(formData.Encode(), "+", "%20", -1)).
+		SetBody(strings.Replace(message+fmt.Sprintf("&transactionSignature=%v", payment.TransactionSignature), " ", "%20", -1)).
 		SetHeaders(headers).
 		SetResult(CreateNewPaymentResponse{}).
 		SetError(ErrorResult{}).
